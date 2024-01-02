9072130a9bfbe8c6c1abc1a501a949815a2f4bda
Hafsa <101563334+hafsa-dreamyland@users.noreply.github.com>
2024-01-02 22:11:43 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/* carousel words */
+let words = ['Dreamer', 'Programmer', 'STEM-Major', 'Automotive', 'Problem solver', 'Leader of myself'];
+let counter = 0;
+let word = document.getElementById("carousel-words");
+setInterval(update_carousel_words, 1000);
+function update_carousel_words(){
+    word.innerHTML = words[counter];
+    counter++;
+    if(counter >= words.length){
+        counter=0;
+    }
+}
+
+//botton to top
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopButton = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 100) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
